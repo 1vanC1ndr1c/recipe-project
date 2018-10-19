c0e50c094adc7050bca9110269188578a69e5722
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-19 22:26:23 +0200

    Modified IndexController

--- src/main/java/com/ic/recipeproject/Repositories/UnitOfMeasureRepository.java
+++ src/main/java/com/ic/recipeproject/Repositories/UnitOfMeasureRepository.java
@@ -1,7 +1,12 @@
 package com.ic.recipeproject.Repositories;
 
+
 import com.ic.recipeproject.domain.UnitOfMeasure;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
+
 public interface UnitOfMeasureRepository extends CrudRepository<UnitOfMeasure, Long> {
+
+    Optional<UnitOfMeasure> findByDescription(String description);
 }
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -7,4 +7,4 @@ INSERT INTO unit_of_measure (description) VALUES ('Tablespoon');
 INSERT INTO unit_of_measure (description) VALUES ('Cup');
 INSERT INTO unit_of_measure (description) VALUES ('Pinch');
 INSERT INTO unit_of_measure (description) VALUES ('Ounce');
---picked up by spring boot
+/*picked up by spring boot*/
