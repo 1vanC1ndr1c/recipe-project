eac96ae592b541201ccd791fb4dd735e6bb1d68b
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-19 20:59:45 +0200

    Add UnitOfMeasure

--- src/main/java/com/ic/recipeproject/domain/Ingredient.java
+++ src/main/java/com/ic/recipeproject/domain/Ingredient.java
@@ -12,11 +12,50 @@ public class Ingredient {
     private String description;
     private BigDecimal amount;
 
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)//it's gonna be retrieved every time from the DB
+    private UnitOfMeasure uom;
 
     //bidirectional
     @ManyToOne//if we delete an ingredient, we don't want to delete recipe
     private Recipe recipe;
 
+    public Long getId() {
+        return id;
+    }
 
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
+    }
+
+    public Recipe getRecipe() {
+        return recipe;
+    }
+
+    public void setRecipe(Recipe recipe) {
+        this.recipe = recipe;
+    }
 }
