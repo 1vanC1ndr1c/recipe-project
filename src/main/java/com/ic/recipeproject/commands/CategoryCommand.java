579a8eb12333f607d452deada2af1b44c37e4c8a
Ivan Cindric <ivan.cindric95@gmail.com>
2018-10-30 17:11:02 +0100

    Small refactor before implementing MySQL

--- src/main/java/com/ic/recipeproject/commands/CategoryCommand.java
+++ src/main/java/com/ic/recipeproject/commands/CategoryCommand.java
@@ -5,6 +5,13 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+
+/**
+ * SPring MVC
+ *
+ * Form will bind properties to commands,
+ * and that will be passed to our controllers
+ */
 @Setter
 @Getter
 @NoArgsConstructor
